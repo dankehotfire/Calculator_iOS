027f4acb0c7776d8a1cf75cb5640fe4d4ce4e970
Danil Nurgaliev <Danilkchik@gmail.com>
2021-03-31 12:20:07 +0300

    code style changed

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -8,8 +8,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var resultLabel: UILabel!
-    
     var firstDigitEntered = false
     var firstNumber: Double = 0
     var secondNumber: Double = 0
@@ -28,10 +26,13 @@ class ViewController: UIViewController {
             }
         }
     }
-    
-    @IBAction func numberPressed(_ sender: UIButton) {
-        guard let number = sender.currentTitle else { return }
-        
+    @IBOutlet weak var resultLabel: UILabel!
+
+    @IBAction private func numberPressed(_ sender: UIButton) {
+        guard let number = sender.currentTitle else {
+            return
+        }
+
         if firstDigitEntered {
             if resultLabel.text!.count < 9 {
                 resultLabel.text = resultLabel.text! + number
@@ -41,17 +42,20 @@ class ViewController: UIViewController {
             firstDigitEntered = true
         }
     }
-    
-    @IBAction func twoNumbersOperation(_ sender: UIButton) {
-        typeOfOperation = sender.currentTitle!
+
+    @IBAction private func twoNumbersOperation(_ sender: UIButton) {
+        guard let senderCurrentTitle = sender.currentTitle else {
+            return
+        }
+        typeOfOperation = senderCurrentTitle
         firstNumber = currentInput
         firstDigitEntered = false
     }
-    
-    @IBAction func signEquallyPressed(_ sender: UIButton) {
+
+    @IBAction private func signEquallyPressed(_ sender: UIButton) {
         secondNumber = currentInput
         firstDigitEntered = false
-        
+
         switch typeOfOperation {
         case "+":
             operationOnTwoNumbers { $0 + $1 }
@@ -65,37 +69,36 @@ class ViewController: UIViewController {
             break
         }
     }
-    
-    @IBAction func allClearButtonPressed(_ sender: UIButton) {
+
+    @IBAction private func allClearButtonPressed(_ sender: UIButton) {
         currentInput = 0
         firstNumber = 0
         secondNumber = 0
         typeOfOperation = ""
         firstDigitEntered = false
     }
-    
-    @IBAction func plusMinusButtonPressed(_ sender: UIButton) {
+
+    @IBAction private func plusMinusButtonPressed(_ sender: UIButton) {
         if resultLabel.text == "0" || resultLabel.text == "0.0" {
             resultLabel.text = "0"
         } else {
             currentInput = -currentInput
         }
     }
-    
-    @IBAction func percentageButtonPressed(_ sender: UIButton) {
+
+    @IBAction private func percentageButtonPressed(_ sender: UIButton) {
         currentInput /= 100
     }
-    
-    @IBAction func dotButtonPressed(_ sender: UIButton) {
+
+    @IBAction private func dotButtonPressed(_ sender: UIButton) {
         if (resultLabel.text!.contains(".")) {
             resultLabel.text = resultLabel.text
         } else {
             resultLabel.text! += "."
         }
     }
-    
+
     func operationOnTwoNumbers(numbers: (Double, Double) -> Double) {
-        currentInput = numbers(firstNumber,secondNumber)
+        currentInput = numbers(firstNumber, secondNumber)
     }
 }
-
